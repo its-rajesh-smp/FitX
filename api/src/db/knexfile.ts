a6317dc15b217a7406b6f9bbc789973fb73c5c7b
its-rajesh-smp <its.rajeshsmp@gmail.com>
2026-06-10 00:38:53 +0530

    chore: updated knexfile

--- api/src/db/knexfile.ts
+++ api/src/db/knexfile.ts
@@ -5,12 +5,17 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const connection = {
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT || 5432),
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE || process.env.POSTGRES_DB,
+  ...(isProduction && {
+    ssl: { rejectUnauthorized: false },
+  }),
 };
 
 const config: Knex.Config = {
